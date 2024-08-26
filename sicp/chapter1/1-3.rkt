b7da2d5b163ea381f305f71ef1675a60d98c886f
uchitsa <uchitsa@ya.ru>
2024-08-27 02:13:16 +0300

    add 1-3.rkt
    Сумма квадратов 
    Определите процедуру solution , которая принимает в качестве аргументов три числа и возвращает сумму квадратов двух больших из них.

---
+++ sicp/chapter1/1-3.rkt
@@ -0,0 +1,9 @@
+#Сумма квадратов 
+#Определите процедуру solution , которая принимает в качестве аргументов три числа и возвращает сумму квадратов двух больших из них.
+(define (solution a b c)
+  (let ((max1 (max a b c))
+        (max2 (max (min a b) (min b c) (min a c))))
+    (+ (* max1 max1) (* max2 max2))))
+(solution 1 2 3)
+(solution 1 0 2)
+(solution 3 2 1)
