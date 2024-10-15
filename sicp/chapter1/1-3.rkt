60fbc93c0d6d0eb19fb66757817f81468fb7a97e
uchitsa <uchitsa@ya.ru>
2024-10-15 17:15:58 +0300

    Update 1-3.rkt

--- sicp/chapter1/1-3.rkt
+++ sicp/chapter1/1-3.rkt
@@ -2,7 +2,7 @@
 #| Сумма квадратов |#
 #| Определите процедуру solution - принимает в качестве аргументов три числа и возвращает сумму квадратов двух больших из них |#
 (define (solution a b c)
-  (let ((max1 (max a b c))
+  (let [(max1 (max a b c)]
         (max2 (max (min a b) (min b c) (min a c))))
     (+ (* max1 max1) (* max2 max2))))
 (solution 1 2 3)
